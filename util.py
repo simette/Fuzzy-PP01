73b4ad8750cda0167f7f9707f9408205cd491824
Gabriely Simette <gaby.simette@gmail.com>
2018-08-11 15:18:12 -0300

    Funcoes usadas por mais de um conjunto

---
+++ util.py
@@ -0,0 +1,11 @@
+# coding: utf-8
+
+def getVariavelLinguistica(numero_conjunto):
+	if (numero_conjunto is str(1)):
+		return 'baixo'
+
+	if (numero_conjunto == str(2)):
+		return 'médio'
+
+	if (numero_conjunto is str(3)):
+		return 'alto'
